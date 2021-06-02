d6ba8d6ae983f4e6cb281d8c68641f8a225c7e7a
5up3r541y4n <karthikuj2001@gmail.com>
2021-06-02 14:02:26 +0530

    Adds SHA-1 verifier

--- hashing/hashsha1.py
+++ hashing/hashsha1.py
@@ -3,4 +3,8 @@ import hashlib, requests, bs4, re, os, sys
 def stringToSHA1(string):
     result = hashlib.sha1(string.encode()) #Create a SHA1 hash object
 
-    return result.hexdigest()
+    return result.hexdigest() #Return the required hexadecimal hash
+
+def verifySHA1(sha1):
+    sha1Regex = re.compile(r'^[a-f0-9]{40}$') #SHA-1 regex object
+    mo = sha1Regex.search(sha1.lower()) #Create a match object
