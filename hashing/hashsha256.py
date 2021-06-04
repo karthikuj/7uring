52867954d9a18ebc51fba0f8c64759322b8d073d
5up3r541y4n <karthikuj2001@gmail.com>
2021-06-04 22:03:29 +0530

    Adds verifySHA256 function

--- hashing/hashsha256.py
+++ hashing/hashsha256.py
@@ -11,3 +11,12 @@ def stringToSHA256(string):
     result = hashlib.sha256(string.encode()) #Create a SHA256 hash object
 
     return result.hexdigest() #Return the required hexadecimal hash
+
+def verifySHA256(sha256):
+    sha256Regex = re.compile(r'^[0-9a-f]{64}$') #Create a regex object
+    mo = sha256Regex.search(sha256.lower()) #Create a match object
+
+    if mo == None:
+        return False
+    else:
+        return True
