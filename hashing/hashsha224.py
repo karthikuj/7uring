6039f94a1ad420b4bb8ae89652ced306569ae34a
5up3r541y4n <karthikuj2001@gmail.com>
2021-06-05 17:36:38 +0530

    Starts verifySHA224 function

--- hashing/hashsha224.py
+++ hashing/hashsha224.py
@@ -11,3 +11,5 @@ def stringToSHA224(string):
     result = hashlib.sha224(string.encode()) #Create a sha224 hash object
 
     return result.hexdigest() #Return the required hexadecimal hash
+
+def verifySHA224(sha224):
