0364fee0f67d5d04d4e5d4305ae16daba6a742b5
5up3r541y4n <karthikuj2001@gmail.com>
2021-06-05 17:33:59 +0530

    Adds colors to hashsha224

--- hashing/hashsha224.py
+++ hashing/hashsha224.py
@@ -1,5 +1,11 @@
 import os, re, sys, bs4, requests, hashlib
 
+colors = {
+    'error':'\033[31;1m[x] ',
+    'success':'\033[36;1m[-] ',
+    'msg':'\033[33;1m[o] '
+    }
+
 def stringToSHA224(string):
 
     result = hashlib.sha224(string.encode()) #Create a sha224 hash object
