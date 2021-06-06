9ad10c53917ac75bac38ce393c1fe620b0c31f40
5up3r541y4n <karthikuj2001@gmail.com>
2021-06-06 08:59:58 +0530

    Completes verifySHA224 function

--- hashing/hashsha224.py
+++ hashing/hashsha224.py
@@ -13,3 +13,10 @@ def stringToSHA224(string):
     return result.hexdigest() #Return the required hexadecimal hash
 
 def verifySHA224(sha224):
+    sha224Regex = re.compile(r'^[0-9a-f]{56}$') #Create a regex object
+    mo = sha224Regex.search(sha224.lower()) #Create a match object
+
+    if mo == None:
+        return False
+    else:
+        return True
