f67a46f28c7010faf46d60752f68049854d04f3d
5up3r541y4n <karthikuj2001@gmail.com>
2021-06-08 21:12:46 +0530

    Adds verifySHA384 function

--- hashing/hashsha384.py
+++ hashing/hashsha384.py
@@ -11,3 +11,12 @@ def stringToSHA384(string):
     result = hashlib.sha384(string.encode()) #Create a SHA384 object
 
     return result.hexdigest() #Return the required hexadecimal hash
+
+def verifySHA384(sha384):
+    sha384Regex = re.compile(r'^[0-9a-f]{96}$') #Create a regex object
+    mo = sha384Regex.search(sha384.lower()) #Create a match object
+
+    if mo == None:
+        return False
+    else:
+        return True
