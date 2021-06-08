02f70919d63d427c003df7989e4f4f24e2a7be5c
5up3r541y4n <karthikuj2001@gmail.com>
2021-06-08 21:30:46 +0530

    minor changes for testing

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from hashing.hashmd5 import *
 from hashing.hashsha1 import *
 from hashing.hashsha256 import *
+from hashing.hashsha224 import *
+from hashing.hashsha384 import *
 
-sha256ToString('8fced00b6ce281456d69daef5f2b33eaf1a4a29b5923ebe5f1f2c54f5886c7a3')
+sha384Brute('de41efa2be0844783ea107630a79246fb1f7b3ab97b35c5e4b70130804f876f64b645c1064a775507a7ac3be457539f2', '/usr/share/wordlists/rockyou.txt')
