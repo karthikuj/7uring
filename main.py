ef9e51d9fd415cde7c3dfb93db7c58b0fe283b81
5up3r541y4n <karthikuj2001@gmail.com>
2021-06-11 18:46:51 +0530

    minor changes

--- main.py
+++ main.py
@@ -3,5 +3,3 @@ from hashing.hashsha1 import *
 from hashing.hashsha256 import *
 from hashing.hashsha224 import *
 from hashing.hashsha384 import *
-
-sha384Brute('de41efa2be0844783ea107630a79246fb1f7b3ab97b35c5e4b70130804f876f64b645c1064a775507a7ac3be457539f2', '/usr/share/wordlists/rockyou.txt')
