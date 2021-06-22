a8c6e0b8338ec50c91a4bf2142517039c4adf399
karthik <karthikuj2001@gmail.com>
2021-06-21 21:09:31 -0700

    Adds colors to main

--- main.py
+++ main.py
@@ -4,4 +4,9 @@ from hashing.hashsha256 import *
 from hashing.hashsha224 import *
 from hashing.hashsha384 import *
 
-print('hello')
+colors = {
+    'error':'\033[31;1m[x] ',
+    'success':'\033[36;1m[-] ',
+    'msg':'\033[33;1m[o] '
+    }
+
