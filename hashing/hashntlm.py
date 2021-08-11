1d51a33834593935301ec59e6f508eea8d6e1dc3
Karthik UJ <karthikuj2001@gmail.com>
2021-08-11 14:32:24 +0530

    Adds ntlm hash module

--- hashing/hashntlm.py
+++ hashing/hashntlm.py
@@ -13,4 +13,48 @@ def stringToNTLM(string):
     return result.hexdigest()
 
 def verifyNTLM(ntlm):
-    
+    ntlmRegex = re.compile(r'^[0-9a-f]{32}$') #Create a regex object
+    mo = ntlmRegex.search(ntlm.lower()) #Create a match object
+
+    if mo == None:
+        return False
+    else:
+        return True
+
+def ntlmToString(ntlm):
+
+    if not verifyNTLM(ntlm):
+        print(colors['error'] + 'Invalid hash')
+        sys.exit()
+
+    else:
+        print(colors['msg'] + 'The online "de-hashing" facility for NTLM\
+ is not available yet,\nyou can still try to bruteforce the hash using 7uring.')
+    sys.exit()
+
+def ntlmBrute(ntlm, wordlist):
+
+    if os.path.exists(wordlist) and os.path.isfile(wordlist): #Check if the wordlist exists
+        if not os.path.isabs(wordlist): #Check if it is an absolute path
+            wordlist = os.path.abspath(wordlist)
+    else:
+        print(colors['error'] + 'Invalid path')
+        sys.exit()
+
+    if not verifyNTLM(ntlm): #Verify if hash is correct
+        print(colors['error'] + 'Invalid hash')
+        sys.exit()
+
+    with open(wordlist, 'r', errors='replace') as w:
+        words = w.readlines() #Store all words in a list
+
+    for word in words:
+        ntlmString = stringToNTLM(word.rstrip())
+
+        if ntlmString == ntlm: #Check if hash matches
+            print(colors['msg'] + 'Cracked!')
+            print(colors['success'] + ntlm + ':' + word)
+            break
+
+    else:
+        print(colors['msg'] + 'Not found')
