853b324bbbb41d801f12cb431911a4c262a08166
Karthik UJ <karthikuj2001@gmail.com>
2021-08-15 17:24:59 +0530

    Adds setup.py file

---
+++ setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="7uring", 
+    version="1.0.0",
+    author="Karthik UJ",
+    author_email="karthikuj2001@gmail.com",
+    description="An advanced cryptography tool.",
+    long_description=' An advanced cryptography tool for hashing, encrypting, encoding, steganography and more.',
+    long_description_content_type="text/markdown",
+    url="https://github.com/karthikuj/7uring",
+    project_urls={
+        "Bug Tracker": "https://github.com/karthikuj/7uring/issues",
+    },
+    entry_points = {
+        'console_scripts': [
+            '7uring = 7uring:main'
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    keywords = ['hashing', 'encoding', 'encryption', 'steganography', 'cryptanalysis', 'steganalysis', 'cipher'],
+    zip_safe = False,
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+)
